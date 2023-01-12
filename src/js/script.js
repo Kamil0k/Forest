f8720420ba728eefa0b281a06327bfff193a5aba
Kamil0k <kamil.porada@onet.pl>
2023-01-12 17:39:24 +0100

    implementation of desktop navigation

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,13 @@
 const hamburgerButton = document.querySelector('.hamburger__button')
-const sideNav = document.querySelector('.side-nav')
-const sideNavLinks = document.querySelectorAll('.side-nav__link')
+const sideNav = document.querySelector('.nav__side')
+const sideNavLinks = document.querySelectorAll('.nav__side-link')
 
 const handleMobileNav = () => {
-	sideNav.classList.toggle('side-nav--active')
+	sideNav.classList.toggle('nav__side--active')
 
 	sideNavLinks.forEach(link => {
 		link.addEventListener('click', () => {
-			sideNav.classList.remove('side-nav--active')
+			sideNav.classList.remove('nav__side--active')
 		})
 	})
 }
