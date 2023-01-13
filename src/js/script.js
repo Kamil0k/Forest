ecbe46389ac732917a0a4e6a361f8f4786fbd084
Kamil0k <kamil.porada@onet.pl>
2023-01-13 23:11:30 +0100

    implemention footer

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 const hamburgerButton = document.querySelector('.hamburger__button')
 const sideNav = document.querySelector('.nav__side')
 const sideNavLinks = document.querySelectorAll('.nav__side-link')
+const year = document.querySelector('.year')
 
 const handleMobileNav = () => {
 	sideNav.classList.toggle('nav__side--active')
@@ -12,4 +13,10 @@ const handleMobileNav = () => {
 	})
 }
 
+const handleYear = () => {
+	const d = new Date()
+	year.innerText = d.getFullYear()
+}
+
+handleYear()
 hamburgerButton.addEventListener('click', handleMobileNav)
